dbadb09d7834738d08dbbbfa3500f2974f7766f5
unglaublichApps <unglaublichApps@gmail.com>
2018-07-17 22:24:17 +0200

    Add new recipe creation with test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
-  get '/recipes', to: 'recipes#index'
+
+  resources :recipes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,8 +17,40 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_url
     assert_template 'recipes/index'
+    assert_select "a[href=?]", recipes_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipes_path(@recipe2), text: @recipe2.name
+  end
+
+  test "should get recipes show" do
+    get recipes_path(@recipe)
+    assert_template 'recipes/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @user.chefname, response.body
+  end
+
+  test "create new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicke, add vegetable, cook 20minutes"
+    assert_difference 'Recipe.count' do
+      post recipes_path, params: {recipe: { name: name_of_recipe, description: description_of_recipe } }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
+
+  test "recet invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " "} }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
 
 end
